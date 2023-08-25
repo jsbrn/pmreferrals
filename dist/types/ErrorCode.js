f88f8ad030b294a1839b983b3d3cbbe282df3b68
Jeremy <23155614+jsbrn@users.noreply.github.com>
2023-08-25 17:36:26 -0400

    Remove dist/ folder

--- dist/types/ErrorCode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorCode = void 0;
-var ErrorCode;
-(function (ErrorCode) {
-})(ErrorCode || (exports.ErrorCode = ErrorCode = {}));
-//# sourceMappingURL=ErrorCode.js.map
